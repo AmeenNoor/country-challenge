a80d1c0e9972e766346cb7f14ae6e33984ec3be3
AmeenNoor <>
2023-07-26 21:59:44 +0000

    Add addLetterToSpot() function to add letter from keyboard to spots

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+let currentRow = 0;
+let currentSpot = 0;
+let rows = document.getElementsByClassName('row');
+let spots = rows[currentRow].children;
+let secret = secretWord();
+// list of countries 
+let wordList = [
+    "ALAND",
+    "ARUBA",
+    "BENIN",
+    "CEUTA",
+    "CHILE",
+    "CHINA",
+    "EGYPT",
+    "GABON",
+    "GHANA",
+    "HAITI",
+    "INDIA",
+    "ITALY",
+    "JAPAN",
+    "KENYA",
+    "LIBYA",
+    "MACAU",
+    "MALTA",
+    "NAURU",
+    "NEPAL",
+    "NIGER",
+    "PALAU",
+    "PAPUA",
+    "QATAR",
+    "SAMOA",
+    "SPAIN",
+    "SUDAN",
+    "SYRIA",
+    "TONGA",
+    "YEMEN"
+];
+
+/** Function to select country from the list randomly */
+function secretWord() {
+    let randomNumber = Math.floor(Math.random() * wordList.length);
+    return wordList[randomNumber];
+}
+
 function checkGuess() {
 
 }
@@ -25,5 +69,8 @@ function removeLetter() {
 }
 
 function addLetterToSpot(letter) {
-
+    if (currentRow < rows.length && currentSpot < spots.length) {
+        spots[currentSpot].textContent = letter;
+        currentSpot++;
+    }
 }
